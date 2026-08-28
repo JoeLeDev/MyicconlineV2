bff7b28da5480a2b400bc35b192ff22ec9fb76d2
joeledev <jonathanluembe@yahoo.com>
2026-08-28 16:32:44 +0200

    fix: New banner FIO

--- src/lib/wp/fio-assets.ts
+++ src/lib/wp/fio-assets.ts
@@ -1,3 +1,3 @@
 /** Bannière officielle de la page liste des groupes FIO (WordPress). */
 export const FIO_GROUPS_PAGE_BANNER =
-  "https://myicconline.com/wp-content/uploads/2026/06/banner-fio.png";
+  "https://myicconline.com/wp-content/uploads/2026/08/Banni%C3%A8re-FIO-Icc.png";
